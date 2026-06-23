afaa9205def7145c494459a78f2fa6bf0ee59e89
Riwaz Mahat <riwazmahat@gmail.com>
2026-06-23 17:30:41 +0545

    Remove unnecessary files and update gitignore

--- check.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Lando Norris"<<endl;
-    return 0;
-}
--- hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>  // Include the input-output stream library
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;  // Print message to the console
-    cout << "Code written by Shreyana" << endl;
-    return 0;  // Exit the program
-}
